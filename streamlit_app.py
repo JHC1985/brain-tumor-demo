e5039b6d95c30e3959058ae55d260eca44a8a6a3
JHC1985 <135380859+JHC1985@users.noreply.github.com>
2026-03-30 17:45:23 +0000

    undate app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Brain Tumor Demo", layout="centered")
+
+st.title("🧠 Brain Tumor Detection Demo")
+st.write("Sube una imagen para probar la interfaz")
+
+uploaded_file = st.file_uploader("Upload image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Imagen subida", use_container_width=True)
+    st.success("Imagen cargada correctamente")
